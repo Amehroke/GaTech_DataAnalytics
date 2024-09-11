fb9de13ac16dce5d4f875f9df54fb578557fc3d2
Amehroke <arashdeepsingh123@gmail.com>
2024-09-11 10:42:11 -0700

    Finished problem 4 & 5

--- HW1/Q5/wrangling_scripts/Q5.py
+++ HW1/Q5/wrangling_scripts/Q5.py
@@ -7,13 +7,14 @@ that will be supplied to the template for generating the table. """
 import csv
 
 def username():
-    return 'gburdell3'
+    return 'Amehroke3'
 
 def data_wrangling():
     with open('data/movies.csv', 'r', encoding='utf-8') as f:
         reader = csv.reader(f)
         table = list()
         # Feel free to add any additional variables
+        table_100 = list()
         ...
         
         # Read in the header
@@ -24,11 +25,14 @@ def data_wrangling():
         for row in reader:
             table.append(row)
             
-            # Only read first 100 data rows - [2 points] Q5.4.a
-            ...
+            if len(table) <= 100:
+                table_100.append(row)
+        
+        
         
         # Order table by the last column - [3 points] Q5.4.b
+        table_100.sort(key=lambda x: float(x[-1]), reverse=True)
         ...
     
-    return header, table
+    return header, table_100
 
